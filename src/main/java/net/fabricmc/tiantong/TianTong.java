de10df50cd4da56826f2ec12210090f906704a15
GBC034 <gbc034@outlook.com>
2023-01-08 14:32:40 +0800

    First Item

---
+++ src/main/java/net/fabricmc/tiantong/TianTong.java
@@ -0,0 +1,16 @@
+package net.fabricmc.tiantong;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TianTong extends Item {
+    public TianTong(Settings settings) {
+		super(settings);
+    }
+}
--- src/main/java/net/fabricmc/tiantong/TiantongMain.java
+++ src/main/java/net/fabricmc/tiantong/TiantongMain.java
@@ -1,21 +1,26 @@
-package net.fabricmc.example;
+package net.fabricmc.tiantong;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class TiantongMain implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("Tiantong");
-
+	public static final Item Oyster = new TianTong(new FabricItemSettings().maxCount(1));
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		Registry.register(Registries.ITEM, new Identifier("tiantong", "oyster"), Oyster);
 		LOGGER.info("Welcome to Tiantong City");
 	}
 }
