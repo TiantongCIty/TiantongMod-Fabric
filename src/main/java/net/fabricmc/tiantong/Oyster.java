d08a1a837f984c819767c5b9dde5b5ccacf60c8c
GBC034 <gbc034@outlook.com>
2023-01-08 22:00:24 +0800

    Add Oyster

--- src/main/java/net/fabricmc/tiantong/Oyster.java
+++ src/main/java/net/fabricmc/tiantong/Oyster.java
@@ -5,12 +5,13 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.scoreboard.Scoreboard;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TianTong extends Item {
-    public TianTong(Settings settings) {
+public class Oyster extends Item {
+    public Oyster(Settings settings) {
 		super(settings);
     }
 }
