81f87b39ba09a2cdd9abd71a7a07a8aa676efbfd
GBC034 <gbc034@outlook.com>
2023-01-13 18:32:01 +0800

    Concrete Stairs

--- src/main/java/net/fabricmc/tiantong/blocks/TBlocks.java
+++ src/main/java/net/fabricmc/tiantong/blocks/TBlocks.java
@@ -14,8 +14,41 @@ import net.minecraft.util.Identifier;
 import static net.fabricmc.tiantong.TiantongMain.Tiantong;
 
 public class TBlocks {
-    public static final StairsBlock Iron_Stairs = new StairsBlock(Blocks.IRON_BLOCK.getDefaultState(),FabricBlockSettings.of(Material.METAL));
+    public static final StairsBlock Iron_Stairs = new StairsBlock(Blocks.IRON_BLOCK.getDefaultState(), FabricBlockSettings.of(Material.METAL));
+    public static final StairsBlock Black_Concrete_Stairs = new StairsBlock(Blocks.BLACK_CONCRETE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.BLACK_CONCRETE));
+    public static final StairsBlock White_Concrete_Stairs = new StairsBlock(Blocks.WHITE_CONCRETE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.WHITE_CONCRETE));
+    public static final StairsBlock Blue_Concrete_Stairs = new StairsBlock(Blocks.BLUE_CONCRETE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.BLUE_CONCRETE));
+    public static final StairsBlock Cyan_Concrete_Stairs = new StairsBlock(Blocks.CYAN_CONCRETE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.CYAN_CONCRETE));
+    public static final StairsBlock Green_Concrete_Stairs = new StairsBlock(Blocks.GREEN_CONCRETE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.GREEN_CONCRETE));
+    public static final StairsBlock Lime_Concrete_Stairs = new StairsBlock(Blocks.LIME_CONCRETE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.LIME_CONCRETE));
+    public static final StairsBlock Magenta_Concrete_Stairs = new StairsBlock(Blocks.MAGENTA_CONCRETE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.MAGENTA_CONCRETE));
+    public static final StairsBlock Brown_Concrete_Stairs = new StairsBlock(Blocks.BROWN_CONCRETE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.BROWN_CONCRETE));
+    public static final StairsBlock Gray_Concrete_Stairs = new StairsBlock(Blocks.GRAY_CONCRETE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.GRAY_CONCRETE));
+    public static final StairsBlock Orange_Concrete_Stairs = new StairsBlock(Blocks.ORANGE_CONCRETE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.ORANGE_CONCRETE));
+    public static final StairsBlock Pink_Concrete_Stairs = new StairsBlock(Blocks.PINK_CONCRETE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.PINK_CONCRETE));
+    public static final StairsBlock Red_Concrete_Stairs = new StairsBlock(Blocks.RED_CONCRETE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.RED_CONCRETE));
+    public static final StairsBlock Purple_Concrete_Stairs = new StairsBlock(Blocks.PURPLE_CONCRETE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.PURPLE_CONCRETE));
+    public static final StairsBlock Yellow_Concrete_Stairs = new StairsBlock(Blocks.YELLOW_CONCRETE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.YELLOW_CONCRETE));
+    public static final StairsBlock Light_Blue_Concrete_Stairs = new StairsBlock(Blocks.LIGHT_BLUE_CONCRETE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.LIGHT_BLUE_CONCRETE));
+    public static final StairsBlock Light_Gray_Concrete_Stairs = new StairsBlock(Blocks.LIGHT_GRAY_CONCRETE.getDefaultState(), FabricBlockSettings.copyOf(Blocks.LIGHT_GRAY_CONCRETE));
+
     public static final void InitBlocks() {
         Registry.register(Registries.BLOCK, new Identifier("tiantong", "iron_stairs"), Iron_Stairs);
+        Registry.register(Registries.BLOCK, new Identifier("tiantong", "black_concrete_stairs"), Black_Concrete_Stairs);
+        Registry.register(Registries.BLOCK, new Identifier("tiantong", "blue_concrete_stairs"), Blue_Concrete_Stairs);
+        Registry.register(Registries.BLOCK, new Identifier("tiantong", "brown_concrete_stairs"), Brown_Concrete_Stairs);
+        Registry.register(Registries.BLOCK, new Identifier("tiantong", "light_blue_concrete_stairs"), Light_Blue_Concrete_Stairs);
+        Registry.register(Registries.BLOCK, new Identifier("tiantong", "light_gray_concrete_stairs"), Light_Gray_Concrete_Stairs);
+        Registry.register(Registries.BLOCK, new Identifier("tiantong", "lime_concrete_stairs"), Lime_Concrete_Stairs);
+        Registry.register(Registries.BLOCK, new Identifier("tiantong", "red_concrete_stairs"), Red_Concrete_Stairs);
+        Registry.register(Registries.BLOCK, new Identifier("tiantong", "pink_concrete_stairs"), Pink_Concrete_Stairs);
+        Registry.register(Registries.BLOCK, new Identifier("tiantong", "purple_concrete_stairs"), Purple_Concrete_Stairs);
+        Registry.register(Registries.BLOCK, new Identifier("tiantong", "yellow_concrete_stairs"), Yellow_Concrete_Stairs);
+        Registry.register(Registries.BLOCK, new Identifier("tiantong", "white_concrete_stairs"), White_Concrete_Stairs);
+        Registry.register(Registries.BLOCK, new Identifier("tiantong", "cyan_concrete_stairs"), Cyan_Concrete_Stairs);
+        Registry.register(Registries.BLOCK, new Identifier("tiantong", "gray_concrete_stairs"), Gray_Concrete_Stairs);
+        Registry.register(Registries.BLOCK, new Identifier("tiantong", "green_concrete_stairs"), Green_Concrete_Stairs);
+        Registry.register(Registries.BLOCK, new Identifier("tiantong", "magenta_concrete_stairs"), Magenta_Concrete_Stairs);
+        Registry.register(Registries.BLOCK, new Identifier("tiantong", "orange_concrete_stairs"), Orange_Concrete_Stairs);
     }
 }
--- src/main/java/net/fabricmc/tiantong/items/TItems.java
+++ src/main/java/net/fabricmc/tiantong/items/TItems.java
@@ -2,6 +2,9 @@ package net.fabricmc.tiantong.items;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.StairsBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.fabricmc.tiantong.blocks.*;
@@ -13,13 +16,61 @@ import static net.fabricmc.tiantong.TiantongMain.Tiantong;
 
 public class TItems {
     public static final Item Oyster = new Oyster(new FabricItemSettings().maxCount(1));
-    public static final Item Iron_Stiars_Item = new BlockItem(TBlocks.Iron_Stairs, new FabricItemSettings());
+    public static final Item Iron_Stairs = new BlockItem(TBlocks.Iron_Stairs, new FabricItemSettings());
+    public static final Item Black_Concrete_Stairs = new BlockItem(TBlocks.Black_Concrete_Stairs, new FabricItemSettings());
+    public static final Item White_Concrete_Stairs = new BlockItem(TBlocks.White_Concrete_Stairs, new FabricItemSettings());
+    public static final Item Blue_Concrete_Stairs = new BlockItem(TBlocks.Blue_Concrete_Stairs, new FabricItemSettings());
+    public static final Item Cyan_Concrete_Stairs = new BlockItem(TBlocks.Cyan_Concrete_Stairs, new FabricItemSettings());
+    public static final Item Green_Concrete_Stairs = new BlockItem(TBlocks.Gray_Concrete_Stairs, new FabricItemSettings());
+    public static final Item Lime_Concrete_Stairs = new BlockItem(TBlocks.Lime_Concrete_Stairs, new FabricItemSettings());
+    public static final Item Magenta_Concrete_Stairs = new BlockItem(TBlocks.Magenta_Concrete_Stairs, new FabricItemSettings());
+    public static final Item Brown_Concrete_Stairs = new BlockItem(TBlocks.Brown_Concrete_Stairs, new FabricItemSettings());
+    public static final Item Gray_Concrete_Stairs = new BlockItem(TBlocks.Gray_Concrete_Stairs, new FabricItemSettings());
+    public static final Item Orange_Concrete_Stairs = new BlockItem(TBlocks.Orange_Concrete_Stairs, new FabricItemSettings());
+    public static final Item Pink_Concrete_Stairs = new BlockItem(TBlocks.Pink_Concrete_Stairs, new FabricItemSettings());
+    public static final Item Red_Concrete_Stairs = new BlockItem(TBlocks.Red_Concrete_Stairs, new FabricItemSettings());
+    public static final Item Purple_Concrete_Stairs = new BlockItem(TBlocks.Purple_Concrete_Stairs, new FabricItemSettings());
+    public static final Item Yellow_Concrete_Stairs = new BlockItem(TBlocks.Yellow_Concrete_Stairs, new FabricItemSettings());
+    public static final Item Light_Blue_Concrete_Stairs = new BlockItem(TBlocks.Light_Blue_Concrete_Stairs, new FabricItemSettings());
+    public static final Item Light_Gray_Concrete_Stairs = new BlockItem(TBlocks.Light_Gray_Concrete_Stairs, new FabricItemSettings());
     public static final void InitItems() {
         Registry.register(Registries.ITEM, new Identifier("tiantong", "oyster"), Oyster);
-        Registry.register(Registries.ITEM, new Identifier("tiantong", "iron_stairs"), Iron_Stiars_Item);
+        Registry.register(Registries.ITEM, new Identifier("tiantong", "iron_stairs"), Iron_Stairs);
+        Registry.register(Registries.ITEM, new Identifier("tiantong", "black_concrete_stairs"), Black_Concrete_Stairs);
+        Registry.register(Registries.ITEM, new Identifier("tiantong", "blue_concrete_stairs"), Blue_Concrete_Stairs);
+        Registry.register(Registries.ITEM, new Identifier("tiantong", "brown_concrete_stairs"), Brown_Concrete_Stairs);
+        Registry.register(Registries.ITEM, new Identifier("tiantong", "light_blue_concrete_stairs"), Light_Blue_Concrete_Stairs);
+        Registry.register(Registries.ITEM, new Identifier("tiantong", "light_gray_concrete_stairs"), Light_Gray_Concrete_Stairs);
+        Registry.register(Registries.ITEM, new Identifier("tiantong", "lime_concrete_stairs"), Lime_Concrete_Stairs);
+        Registry.register(Registries.ITEM, new Identifier("tiantong", "red_concrete_stairs"), Red_Concrete_Stairs);
+        Registry.register(Registries.ITEM, new Identifier("tiantong", "pink_concrete_stairs"), Pink_Concrete_Stairs);
+        Registry.register(Registries.ITEM, new Identifier("tiantong", "purple_concrete_stairs"), Purple_Concrete_Stairs);
+        Registry.register(Registries.ITEM, new Identifier("tiantong", "yellow_concrete_stairs"), Yellow_Concrete_Stairs);
+        Registry.register(Registries.ITEM, new Identifier("tiantong", "white_concrete_stairs"), White_Concrete_Stairs);
+        Registry.register(Registries.ITEM, new Identifier("tiantong", "cyan_concrete_stairs"), Cyan_Concrete_Stairs);
+        Registry.register(Registries.ITEM, new Identifier("tiantong", "gray_concrete_stairs"), Gray_Concrete_Stairs);
+        Registry.register(Registries.ITEM, new Identifier("tiantong", "green_concrete_stairs"), Green_Concrete_Stairs);
+        Registry.register(Registries.ITEM, new Identifier("tiantong", "magenta_concrete_stairs"), Magenta_Concrete_Stairs);
+        Registry.register(Registries.ITEM, new Identifier("tiantong", "orange_concrete_stairs"), Orange_Concrete_Stairs);
         ItemGroupEvents.modifyEntriesEvent(Tiantong).register(content -> {
-            content.add(TItems.Oyster);
-            content.addAfter(TItems.Oyster, Iron_Stiars_Item);
+            content.add(Oyster);
+            content.addAfter(Oyster, Iron_Stairs);
+            content.addAfter(Iron_Stairs, White_Concrete_Stairs);
+            content.addAfter(White_Concrete_Stairs, Light_Gray_Concrete_Stairs);
+            content.addAfter(Light_Gray_Concrete_Stairs, Gray_Concrete_Stairs);
+            content.addAfter(Gray_Concrete_Stairs, Black_Concrete_Stairs);
+            content.addAfter(Black_Concrete_Stairs, Brown_Concrete_Stairs);
+            content.addAfter(Brown_Concrete_Stairs, Red_Concrete_Stairs);
+            content.addAfter(Red_Concrete_Stairs, Orange_Concrete_Stairs);
+            content.addAfter(Orange_Concrete_Stairs, Yellow_Concrete_Stairs);
+            content.addAfter(Yellow_Concrete_Stairs, Lime_Concrete_Stairs);
+            content.addAfter(Lime_Concrete_Stairs, Green_Concrete_Stairs);
+            content.addAfter(Green_Concrete_Stairs, Cyan_Concrete_Stairs);
+            content.addAfter(Cyan_Concrete_Stairs, Light_Blue_Concrete_Stairs);
+            content.addAfter(Light_Blue_Concrete_Stairs, Blue_Concrete_Stairs);
+            content.addAfter(Blue_Concrete_Stairs, Purple_Concrete_Stairs);
+            content.addAfter(Purple_Concrete_Stairs, Magenta_Concrete_Stairs);
+            content.addAfter(Magenta_Concrete_Stairs, Pink_Concrete_Stairs);
         });
     }
 }
